37e713df18538c1899c83f64ea6e8b702265835b
charitylefosa <charitylefosa@gmail.com>
2023-05-19 06:32:02 -0700

    description

--- shell.c
+++ shell.c
@@ -5,7 +5,11 @@
 #include<stdlib.h>
 
 #define MAX_COMMAND_LENGTH 1024
-
+#define MAX_ARGS 64
+/**
+* command_line - displays prompt for user to type command
+* @command: command line user inputs
+*/
 void command_line(char *command)
 {
 	printf("$ ");
@@ -13,13 +17,29 @@ void command_line(char *command)
 	{
 		printf("\n");
 	}
-	else
-	{
-		exit(EXIT_FAILURE);
-	}
 }
+/**
+* parse_args - handles arguments in a command
+* @command: command line user input
+* @args: arguments to be executed inthe command
+*/
+void parse_args(char *command, char *args[])
+{
+	int i = 0;
 
-void execute_command(char *command)
+	args[i] = strtok(command, " ");
+	while (args[i] != NULL && i < MAX_ARGS - 1)
+	{
+		i++;
+		args[i] = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+}
+/**
+* execute_command - executes commands
+* @args: arguments to be executed inthe command
+*/
+void execute_command(char *args[])
 {
 	pid_t pid;
 
@@ -31,7 +51,7 @@ void execute_command(char *command)
 	}
 	else if (pid == 0)
 	{
-		execlp(command, command, NULL);
+		execvp(args[0], args);
 		fprintf(stderr, "Error: Command not found.\n");
 		exit(EXIT_FAILURE);
 	}
@@ -40,17 +60,31 @@ void execute_command(char *command)
 		wait(NULL);
 	}
 }
-
+/**
+* main - pront is printed, user imputs command, arguments
+* in the command are executed and give output, exit
+* command is to exit the shell.
+* Return: Always 0
+*/
 int main(void)
 {
 	char command[MAX_COMMAND_LENGTH];
+	char *args[MAX_ARGS];
 	int running = 1;
 
 	while (running)
 	{
 		command_line(command);
 		command[strcspn(command, "\n")] = '\0';
-		execute_command(command);
+		if (strcmp(command, "exit") == 0)
+		{
+			running = 0;
+		}
+		else
+		{
+			parse_args(command, args);
+			execute_command(args);
+		}
 	}
 	return (0);
 }
